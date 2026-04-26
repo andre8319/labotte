d1aeee35218050309173a8c73c162a9b7e20cdf9
andre8319 <87731792+andre8319@users.noreply.github.com>
2026-04-26 11:59:57 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors())
 app.use(express.json())
 
 // API routes
